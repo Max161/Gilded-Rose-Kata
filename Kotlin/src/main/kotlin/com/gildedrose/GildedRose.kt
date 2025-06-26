bbe83af974835e8814f9f3e877b9f17dae4db67c
Max <max.vorobey@beyonnex.io>
2025-06-26 17:07:39 +0200

    Kai - Rui - Max first kata!

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,55 +3,64 @@ package com.gildedrose
 class GildedRose(val items: List<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
+        for (item in items) {
+            handleSellIn(item)
         }
+    }
+
+    private fun manageBackstageQuality(item: Item) {
+        if (item.sellIn < 11) {
+            increaseQuality(item)
+        }
+        if (item.sellIn < 6) {
+            increaseQuality(item)
+        }
+        increaseQuality(item)
+    }
+
+    private fun manageExpiredItems(item: Item) =
+        when (item.name) {
+            BACKSTAGE_PASSES -> setMinQuality(item)
+            AGED_BRIE -> increaseQuality(item)
+            SULFURAS -> Unit
+            else -> decreaseQuality(item)
+        }
+
+    private fun handleSellIn(item: Item) {
+        when (item.name) {
+            AGED_BRIE -> increaseQuality(item)
+            BACKSTAGE_PASSES -> manageBackstageQuality(item)
+            SULFURAS -> Unit
+            else -> decreaseQuality(item)
+        }
+
+        if (item.name != SULFURAS) {
+            decreaseSellIn(item)
+        }
+
+        if (item.sellIn < 0) {
+            manageExpiredItems(item)
+        }
+    }
+
+    fun decreaseQuality(item: Item) {
+        if (item.quality > MIN_QUALITY) {
+            item.quality--
+        }
+    }
+
+    fun increaseQuality(item: Item) {
+        if (item.quality < MAX_QUALITY) {
+            item.quality++
+        }
+    }
+
+    private fun setMinQuality(item: Item) {
+        item.quality = MIN_QUALITY
+    }
+
+    private fun decreaseSellIn(item: Item) {
+        item.sellIn = item.sellIn - 1
     }
 
 }
